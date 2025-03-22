5a187e270dfe3671d636f6744a97f1d80903f0ce
Yasusu <138525886+YasusuDev@users.noreply.github.com>
2025-03-22 01:10:28 -0300

    feat: Setup AnimInstances.

---
+++ Source/RPGCombat/Public/AnimInstances/WarriorBaseAnimInstance.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "WarriorBaseAnimInstance.generated.h"
+
+UCLASS()
+class RPGCOMBAT_API UWarriorBaseAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
