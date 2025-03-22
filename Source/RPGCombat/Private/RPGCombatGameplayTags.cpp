7cf2c4fdfb7f3c16df606faa0a3238b6cd77367e
Yasusu <138525886+YasusuDev@users.noreply.github.com>
2025-03-21 23:26:47 -0300

    feat: added module for gameplay tag support and added custom gameplay tags by cpp.

---
+++ Source/RPGCombat/Private/RPGCombatGameplayTags.cpp
@@ -0,0 +1,8 @@
+#include "RPGCombatGameplayTags.h"
+
+namespace WarriorGameplayTags
+{
+	/** Input Tags **/
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move,"InputTag.Move");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look,"InputTag.Look");
+}
---
+++ Source/RPGCombat/Public/RPGCombatGameplayTags.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace WarriorGameplayTags
+{
+	/** Input Tags **/
+	RPGCOMBAT_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
+	RPGCOMBAT_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+}
